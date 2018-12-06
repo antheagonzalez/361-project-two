f9d0931bb4e631c66f6d4fe83e60c38cfa8e2d88
Anthea Gonzalez <agonzalez2@hawk.itt.edu>
2018-12-06 15:31:48 -0600

    Add button

--- site.js
+++ site.js
@@ -1,1 +1,12 @@
 // JS file
+
+
+  // Define the listener and its function
+document.querySelector('.btn').addEventListener('click', function() {
+  // how do I get random num?
+
+  // Define the veriable that change the images
+  var imgLoosy = document.querySelector('.loosy');
+  imgLoosy.src = 'my-cat/loosy-' + imgNum + '.jpg';
+
+});
