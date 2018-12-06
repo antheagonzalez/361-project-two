599ac228687fc1a7dbabde7cfffc7a318ccb6f32
Anthea Gonzalez <agonzalez2@hawk.itt.edu>
2018-12-06 16:13:07 -0600

    All JS that I forgot to commit earlier

--- site.js
+++ site.js
@@ -1,12 +1,28 @@
-// JS file
+// remove the 'nojs' class and add the 'js' class
+var html = document.querySelector('html');
+html.classList.remove('nojs');
+html.classList.add('js');
 
+var navigation = document.querySelector('#nav');
+var nav_list = document.querySelector('.nav-list');
 
-  // Define the listener and its function
-document.querySelector('.btn').addEventListener('click', function() {
-  // how do I get random num?
+// build the outer heading element
+var nav_heading = document.createElement('h3');
 
-  // Define the veriable that change the images
-  var imgLoosy = document.querySelector('.loosy');
-  imgLoosy.src = 'my-cat/loosy-' + imgNum + '.jpg';
+// build the inner link element
+var menu_link = document.createElement('a');
 
+menu_link.textContent = 'Menu';
+menu_link.setAttribute('id', 'menu-button');
+
+// append the menu_link to the heading element
+nav_heading.append(menu_link);
+
+// append the nav_heading to #navigation
+navigation.insertBefore(nav_heading, nav_list);
+
+// Listen for clicks on #menu-button and toggle the visible class
+var menu_button = document.querySelector('#menu-button');
+menu_button.addEventListener('click', function() {
+  nav_list.classList.toggle('visible');
 });
