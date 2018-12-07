91509d484f835270eb47d8689ab1d41065ba905b
Anthea Gonzalez <agonzalez2@hawk.itt.edu>
2018-12-06 20:27:52 -0600

    Fix nav-class to nav-list in CSS fil

--- site.js
+++ site.js
@@ -1,6 +1,5 @@
 // remove the 'nojs' class and add the 'js' class
 var html = document.querySelector('html');
-html.classList.remove('nojs');
 html.classList.add('js');
 
 var navigation = document.querySelector('#nav');
